30acc1bff5a656d21980738f8bd6e774581a96b0
VaskoDeGama <vasr161095@gmail.com>
2020-08-29 01:14:17 +0800

    189 update jsdoc

--- src/189. Rotate Array/rotate.js
+++ src/189. Rotate Array/rotate.js
@@ -1,4 +1,13 @@
 /**
+ * Given an array, rotate the array to the right by k steps, where k is non-negative.
+ * Follow up:
+ * Try to come up as many solutions as you can, there are at least 3
+ * different ways to solve this problem.
+ * Could you do it in-place with O(1) extra space?
+ * Constraints:
+ * 1 <= nums.length <= 2 * 10^4
+ * It's guaranteed that nums[i] fits in a 32 bit-signed integer.
+ * k >= 0
  * @param {number[]} nums
  * @param {number} k
  * @return {void} Do not return anything, modify nums in-place instead.
