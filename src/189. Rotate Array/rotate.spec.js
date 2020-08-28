47c9efe032f21190d0bb42093e9088e9500755a5
VaskoDeGama <vasr161095@gmail.com>
2020-08-29 01:10:08 +0800

    189 added test

--- src/189. Rotate Array/rotate.spec.js
+++ src/189. Rotate Array/rotate.spec.js
@@ -9,16 +9,22 @@ describe('rotate',  () => {
     expect(rotate()).not.toBeDefined()
   })
   test('simple test 1', () => {
-    const nums = [1,2,3,4,5,6,7]
+    const nums1 = [1,2,3,4,5,6,7]
     const k = 3
-    rotate(nums, k)
-    expect(nums).toStrictEqual([5,6,7,1,2,3,4])
+    rotate(nums1, k)
+    expect(nums1).toStrictEqual([5,6,7,1,2,3,4])
   })
   test('simple test 2', () => {
     const nums2 = [-1,-100,3,99]
     const k = 2
     rotate(nums2, k)
-    expect(nums2).toStrictEqual([5,6,7,1,2,3,4])
+    expect(nums2).toStrictEqual([3,99,-1,-100])
+  })
+  test('simple test 3', () => {
+    const nums3 = [-1,-100]
+    const k = 3
+    rotate(nums3, k)
+    expect(nums3).toStrictEqual([-100,-1])
   })
 
 })
