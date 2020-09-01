92d74a7fde8a34b71c26c01abd0b6a8bff2de360
VaskoDeGama <vasr161095@gmail.com>
2020-09-01 22:42:05 +0800

    189 one more

--- src/189. Rotate Array/rotate.js
+++ src/189. Rotate Array/rotate.js
@@ -13,8 +13,9 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 function rotate(nums, k) {
-  if(!nums || nums.length < 1) return
-  nums.unshift(...nums.splice(nums.length  - k, nums.length - 1))
+  while (k--) {
+    nums.unshift(nums.pop())
+  }
 }
 
 module.exports = rotate
