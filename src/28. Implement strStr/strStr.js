f5f7576d8d79f690d8494eb82a9399bc96a2b445
VaskoDeGama <vasr161095@gmail.com>
2020-09-01 20:42:39 +0800

    28 work again

--- src/28. Implement strStr/strStr.js
+++ src/28. Implement strStr/strStr.js
@@ -8,8 +8,13 @@
  * @return {number}
  */
 function strStr(haystack, needle) {
-  if(needle === "") return 0
-  return haystack.indexOf(needle, 0)
+  if(!needle) return 0
+  for (let i = 0; i < haystack.length; i++) {
+    if(haystack.substring(i, i+needle.length) === needle) {
+      return i
+    }
+  }
+  return -1
 }
 
 
