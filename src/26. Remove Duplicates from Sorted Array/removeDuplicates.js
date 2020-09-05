cd2300ac8c9a2f149aa6bbda887f7a9ce3782a39
VaskoDeGama <vasr161095@gmail.com>
2020-09-05 12:36:21 +0800

    26 work:  with Set

--- src/26. Remove Duplicates from Sorted Array/removeDuplicates.js
+++ src/26. Remove Duplicates from Sorted Array/removeDuplicates.js
@@ -7,15 +7,8 @@
  * @return {number}
  */
 function removeDuplicates(nums) {
-  if(nums.length === 0) return 0
-  let i = 0
-  for(let j = 0; j < nums.length;j++) {
-    if( nums[j] !== nums[i] ) {
-      i++
-      nums[i] = nums[j]
-    }
-  }
-  return i + 1
+  nums = [...new Set(nums)]
+  return nums.length
 }
 
 module.exports = removeDuplicates
